d9ca25d5a6ffe102111b4cbe63ca97e42f958b56
zhuzhenjie <mimotronik@gmail.com>
2023-05-29 10:12:44 +0800

    fix: ✨ Content-Type

--- biz-gateway/src/main/java/org/iproute/biz/gateway/filters/reqFilter/header/ReqRequireContentTypeFilter.java
+++ biz-gateway/src/main/java/org/iproute/biz/gateway/filters/reqFilter/header/ReqRequireContentTypeFilter.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -16,17 +17,20 @@ import reactor.core.publisher.Mono;
  * @since 5/8/2023
  */
 @Profile(BizGatewayApplication.PROFILES.ENCRYPT_DECRYPT)
-// @Component
+@Component
 public class ReqRequireContentTypeFilter implements BizGlobalFilter {
 
     @Override
     public Mono<Void> bizFilter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
         MediaType contentType = request.getHeaders().getContentType();
-        if (contentType == null) {
+        if (contentType == null
+                || MediaType.APPLICATION_JSON.includes(contentType)
+                || MediaType.TEXT_PLAIN.includes(contentType)) {
+            return chain.filter(exchange);
+        } else {
             return Mono.error(new RuntimeException("lost Content-Type"));
         }
-        return chain.filter(exchange);
     }
 
     @Override
